4187c01de15b8b06396baa44be29933e5d8e00bd
Sabrina Ahmad <saabriinaahmad@gmail.com>
2025-09-14 04:53:58 +0300

    remove @vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+     <title>{{ config('app.name', 'Cardify || Welcome') }}</title>
 
-    {{-- <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))) --}}
-    {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
 </head>
 
-<!-- <body class="bg-light-blue"> -->
-    <body>
+<body>
     <div class="container-fluid background-layout">
         <div class="row h-100">
             <div class="col-8 bg-left"></div>
